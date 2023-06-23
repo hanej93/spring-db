254eb611cf2cb5fd7de4d1ef55b7e3329d455483
EJ <hanej93@naver.com>
2023-06-23 12:52:58 +0900

    JDBC 개발 - 수정, 삭제

--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -3,6 +3,7 @@ package hello.jdbc.repository;
 import static org.assertj.core.api.Assertions.*;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -27,5 +28,15 @@ class MemberRepositoryV0Test {
 		log.info("member == findMember {}", member == findMember);
 		log.info("member.equals(findMember) {}", member.equals(findMember));
 		assertThat(findMember).isEqualTo(member);
+
+		// update: money: 10000 -> 20000
+		repository.update(member.getMemberId(), 20000);
+		Member updatedMember = repository.findById(member.getMemberId());
+		assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+		// delete
+		repository.delete(member.getMemberId());
+		assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+			.isInstanceOf(NoSuchElementException.class);
 	}
 }
